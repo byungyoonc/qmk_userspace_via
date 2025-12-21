b70ad4f7cbab6fad940e24868425b76ceb4c8292
Choi Byungyoon <byungyoonc@gmail.com>
2025-12-21 17:01:23 +0900

    Add personal keymap for IDOBAO Montex v1

---
+++ keyboards/idobao/montex/v1/keymaps/byungyoonc/config.h
@@ -0,0 +1,11 @@
+// Copyright 2022 IBNobody (@IBNobody)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+#ifdef DEBOUNCE
+#undef DEBOUNCE
+#endif
+#define DEBOUNCE 40
