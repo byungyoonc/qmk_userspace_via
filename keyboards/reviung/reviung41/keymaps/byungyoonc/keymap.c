14beff29477a36180734bd35261389f182ab7da1
Choi Byungyoon <byungyoonc@gmail.com>
2025-12-22 07:34:52 +0900

    Add personal keyamp for Reviung41

---
+++ keyboards/reviung/reviung41/keymaps/byungyoonc/keymap.c
@@ -0,0 +1,109 @@
+/* Copyright 2020 gtips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "keycodes.h"
+#include QMK_KEYBOARD_H
+#include "byungyoonc.h"
+
+enum layer_names {
+    _BASE,
+    _LOWER,
+    _NUMPAD,
+    _RAISE,
+    _ADJUST
+};
+
+#define LOWER  MO(_LOWER)
+#define NUMPAD MO(_NUMPAD)
+#define RAISE  MO(_RAISE)
+#define AGRGUI LGUI_T(KC_RALT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_reviung41(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                        KC_LALT, LOWER,   KC_SPC,  RAISE,   AGRGUI
+  ),
+
+  [_LOWER] = LAYOUT_reviung41(
+    KC_F1,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_CAPS,          KC_NUM,  KC_7,    KC_8,    KC_9,    KC_PMNS, KC_DEL,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,          XXXXXXX, KC_4,    KC_5,    KC_6,    KC_PPLS, _______,
+    _______, KC_ESC,  XXXXXXX, XXXXXXX, NUMPAD,  KC_APP,           XXXXXXX, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
+                                        _______, _______, KC_0,    _______, _______
+  ),
+
+  [_NUMPAD] = LAYOUT_reviung41(
+    _______, _______, _______, _______, _______, _______,          _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+                                        _______, _______, KC_P0,   _______, _______
+  ),
+
+  [_RAISE] = LAYOUT_reviung41(
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_PERC, XXXXXXX,          XXXXXXX, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  KC_DEL,
+    _______, KC_AMPR, KC_ASTR, KC_DLR,  XXXXXXX, XXXXXXX,          XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______,
+    _______, XXXXXXX, KC_CIRC, XXXXXXX, XXXXXXX, KC_APP,           XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, _______,
+                                        _______, _______, _______, _______, _______
+  ),
+
+  [_ADJUST] = LAYOUT_reviung41(
+    _______, UG_TOGG, KC_MPRV, KC_MPLY, KC_MNXT, KC_SCRL,          KC_VOLU, KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_PSCR,
+    _______, KC_SEC1, KC_SEC2, XXXXXXX, XXXXXXX, XXXXXXX,          KC_VOLD, KC_F4,   KC_F5,   KC_F6,   KC_F11, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NK_TOGG,          KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F12, _______,
+                                        _______, _______, QK_BOOT, _______, _______
+  ),
+};
+
+void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
+    for (int i = 0; i < 5; i++) {
+        waitInputPinDelay();
+    }
+}
+
+const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, 0, 0, 0}
+);
+const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, 169, 255, 255}
+);
+const rgblight_segment_t PROGMEM raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, 21, 255, 255}
+);
+const rgblight_segment_t PROGMEM adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, 64, 255, 255}
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    base_layer,
+    lower_layer,
+    raise_layer,
+    adjust_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = rgb_layers;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+    rgblight_set_layer_state(0, layer_state_cmp(state, _BASE));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
+    return state;
+}
